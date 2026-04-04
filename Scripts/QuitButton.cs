476bddb999a12d1ae0ab22025f13f7a7d06f4268
Donald <dwd1025@unh.edu>
2026-04-04 19:19:29 -0400

    added confimation before quittting to main menu

--- Scripts/QuitButton.cs
+++ Scripts/QuitButton.cs
@@ -14,13 +14,35 @@ public partial class QuitButton : Button
 	{
 	}
 
+	//debug method to print the scene tree. Can be deleted later.
+	private void PrintTree(Node node, string indent = "")
+	{
+		GD.Print(indent + node.Name);
+
+		foreach (Node child in node.GetChildren())
+			PrintTree(child, indent + "  ");
+	}
+
+
 	private void _on_QuitButton_pressed()
 	{
-		// Free all placed turrets
+
+		var dialog = GetNode<ConfirmationDialog>("../../../QuitConfirmDialog");
+		//GD.Print("Dialog found: ", dialog);
+		//GD.Print("QuitButton path: ", GetPath());
+		//PrintTree(GetTree().Root);
+
+
+		dialog.PopupCentered();
+
+	}
+
+	private void _on_quit_confirm_dialog_confirmed()
+	{
 		foreach (Node turret in GetTree().GetNodesInGroup("placed_turrets"))
 			turret.QueueFree();
 
-		GD.Print(GetPath()); 
 		GetTree().ChangeSceneToFile("res://Scenes/start_menu.tscn");
 	}
+
 }
