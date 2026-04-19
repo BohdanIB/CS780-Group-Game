f1734ba4cab14f51cf49a3fa37c22616170bdb48
njwilder <49539716+njwilder@users.noreply.github.com>
2026-04-19 10:46:13 -0400

    Revert "Merge pull request #30 from BohdanIB/revert-29-28-expand-structure-system"
    This reverts commit 25795cd959b057416622fd6565d501a66c540bd5, reversing
    changes made to 14d9e81be8434969ea6e703f4a7a007e45b6fde4.

---
+++ Scripts/OptionSelector.cs
@@ -0,0 +1,59 @@
+using Godot;
+using System;
+
+public partial class OptionSelector : Panel
+{
+    [Export] private TextureButton _leftButton, _rightButton;
+    [Export] private Label _selectionLabel, _nameLabel;
+
+    public string OptionsName {get; private set;}
+    private string[] _options;
+    private int _currentIndex;
+
+    [Signal] public delegate void OnIndexChangedEventHandler(int index);
+
+
+    public void SetOptions(string[] options, string name, int index=0)
+    {
+        this._options = options;
+        this.OptionsName = name;
+        _nameLabel.Text = name;
+        index = Math.Max(0, Math.Min(options.Length-1, index));
+
+        if (options != null && options.Length > 0)
+        {
+            Visible = true;
+            UpdateDisplay();
+        } else
+        {
+            Visible = false;
+        }
+        
+    }
+
+    private void UpdateDisplay()
+    {
+        _selectionLabel.Text = _options[_currentIndex];
+        _leftButton.Visible = _currentIndex > 0;
+        _rightButton.Visible = _currentIndex < _options.Length-1;
+    }
+
+    public void IncreaseIndex()
+    {
+        _currentIndex = Math.Min(_options.Length-1, _currentIndex+1);
+        EmitSignal(SignalName.OnIndexChanged, _currentIndex);
+        UpdateDisplay();
+    }
+
+    public void DecreaseIndex()
+    {
+        _currentIndex = Math.Max(0, _currentIndex-1);
+        EmitSignal(SignalName.OnIndexChanged, _currentIndex);
+        UpdateDisplay();
+    }
+
+    public string GetOptionSelection()
+    {
+        return _options[_currentIndex];
+    }
+}
